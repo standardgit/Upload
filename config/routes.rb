77c012c1f1348fed58030195e048c3d52b7faa9d
standardgit <standardblaise@gmail.com>
2022-03-05 12:06:50 +0100

    Edit  page styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users,
     path: '',
     path_names: {sign_in: "login", sign_out: "logout", edit: "profile", sign_up: "registration"},
-    controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'users/registrations'}
+    controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
   #  For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :users, only: [:show]
+  resources :users, only: [:show]
 end
